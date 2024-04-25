4a4be35f63ccddbf39caa8f7f5b49cdd58447325
jpcutler <jpcutler@coastal.edu>
2024-04-24 22:43:39 -0400

    initial commit

---
+++ F#Tutorial/Program.fs
@@ -0,0 +1,20 @@
+﻿open System.IO
+
+let deleteFolder (folderPath: string) =
+
+    try
+
+        Directory.Delete(folderPath, true)
+        printfn "Directory Deleted"
+
+    with
+    | :? DirectoryNotFoundException ->
+
+        printfn "Directory doesnt exist"
+
+    | :? IOException as ex ->
+
+        printfn "Error deleting folder '%s': %s" folderPath ex.Message
+
+let folderPath = "testfolder"
+deleteFolder folderPath
